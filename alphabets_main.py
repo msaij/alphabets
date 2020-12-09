009e8bb96c758b7e20d4dbffc2a325eb0aa01b72
MSaiJagapathi <msaij@users.noreply.github.com>
2020-12-09 18:33:05 +0530

    nextstep - taking a screenshot on key command

--- alphabets_main.py
+++ alphabets_main.py
@@ -1,10 +1,11 @@
 import os
+from os import path
 import pyautogui
 
 folderofpic="C:/Users/SpectrusRage/3D Objects/alphabets/oneimg_ss"
 imgname="theoneandonlyimg.png"
 
-def onlyoneimgcheck():
+def excptmainimg_del_evryothrfile():
     for thefile in os.listdir(folderofpic):
         if thefile==imgname:
             pass
@@ -15,6 +16,7 @@ def onlyoneimgcheck():
                 os.rmdir(os.path.join(folderofpic,thefile))
 
 def saveimg():
+    print("Which image you want as your screen lock")
     myScreenshot = pyautogui.screenshot() #screenshot is taken and saved in myScreenshot
     imgpath=os.path.join(folderofpic,imgname)
     myScreenshot.save(imgpath)
@@ -24,8 +26,8 @@ def mainfn_saveandcheck():
     if len(os.listdir(folderofpic))==0:
         saveimg()
     else:
-        onlyoneimgcheck()
+        excptmainimg_del_evryothrfile()
         if len(os.listdir(folderofpic))==0:
             saveimg()
 
-mainfn_saveandcheck() # Running the main function which has both saveimg() and onlyoneimgcheck()
+mainfn_saveandcheck() # Running the main function which has both saveimg() and excptmainimg_del_evryothrfile()
--- drags.py
+++ drags.py
@@ -1,14 +1,1 @@
 import os
-
-def fc():
-    filescountpath=os.listdir("C:/Users/SpectrusRage/3D Objects/alphabets/oneimg_ss")
-    filescount=0
-    for a_file in filescountpath:
-        filescount+=1
-    return filescount
-
-filescount=fc()
-if filescount==1:
-    print("swipe code")
-else:
-    os.system("python alphabets_main.py")
